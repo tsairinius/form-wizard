3536f18d232952906f9ab7d5d74b98fc3b2e0038
Eric T <9063787+tsairinius@users.noreply.github.com>
2020-11-08 11:17:18 -0800

    Validate password with UI highlighting

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './ContactForm.css';
 
-const ContactForm = ({onChange, contactInfo}) => {
+const ContactForm = ({onChange, contactInfo, isPasswordMatched}) => {
     return (
-        <form className="contact-form">
-            <label htmlFor="first">First</label>
-            <input type="text" id="first" name="first" value={contactInfo.first} onChange={onChange}/>
-            <label htmlFor="last">Last</label>
-            <input type="text" id="last" name="last" value={contactInfo.last} onChange={onChange}/>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" value={contactInfo.email} onChange={onChange}/>
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" value={contactInfo.password} onChange={onChange}/>
-            <label htmlFor="confirmPass">Confirm Password</label>
-            <input type="password" id="confirmPass" name="confirmPass" value={contactInfo.confirmPass} onChange={onChange}/>
-        </form>
+        <div>
+            <p className="form-instructions">Please fill out all fields below</p>
+            <form className="contact-form">
+                <label htmlFor="first">First</label>
+                <input type="text" id="first" name="first" value={contactInfo.first} onChange={onChange}/>
+                <label htmlFor="last">Last</label>
+                <input type="text" id="last" name="last" value={contactInfo.last} onChange={onChange}/>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" value={contactInfo.email} onChange={onChange}/>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" value={contactInfo.password} onChange={onChange} className={!isPasswordMatched ? 'input-error' : undefined}/>
+                <label htmlFor="confirmPass">Confirm Password</label>
+                <input type="password" id="confirmPass" name="confirmPass" value={contactInfo.confirmPass} onChange={onChange} className={!isPasswordMatched ? 'input-error' : undefined}/>
+            </form>
+            <div className="error-messages">
+                {!isPasswordMatched ? <p>Password mismatch</p> : undefined}
+            </div>
+        </div>
     )
 }
 
