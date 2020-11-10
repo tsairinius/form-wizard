db1d48b3e9b661273ec81a4e997a4281f725bae6
Eric T <9063787+tsairinius@users.noreply.github.com>
2020-11-10 10:37:54 -0800

    Refactor App -Created custom hook for contact info

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import Wizard from './components/Wizard';
-import StyledContainer from './components/StyledContainer';
-import './App.css';
-import ContactForm from './components/ContactForm/ContactForm';
+import useContactInfo from './hooks/useContactInfo';
+import ContactForm from './components/ContactForm';
 import UserAvailability from './components/UserAvailability';
+import StyledContainer from './components/StyledContainer';
+import Wizard from './components/Wizard';
+import './App.css';
 
 function App() {
-  const initialInfo = {
-    first: "",
-    last: "",
-    email: "",
-    password: "",
-    confirmPass: ""
-  }
-
-  const [ contactInfo, setContactInfo ] = useState(initialInfo);
-  const [ isPasswordMatched, setIsPasswordMatched ] = useState(true);
-  const [ canProgress, setCanProgress ] = useState(true);
-  const [ currentStep, setCurrentStep ] = useState(0);
+  const { contactInfo,
+          updateContactInfo, 
+          isPasswordMatched,
+          updateIsPasswordMatched,
+          checkForValidForm
+        } = useContactInfo();
 
   const [availability, setAvailability ] = useState([]);
+  const [ canProgress, setCanProgress ] = useState(true);
+  const [ currentStep, setCurrentStep ] = useState(0);
 
   useEffect(() => {
     updateIsPasswordMatched();
@@ -44,44 +41,6 @@ function App() {
 
   const updateAvailability = (updatedAvail) => {
     setAvailability(updatedAvail);
-  }
-
-  const updateContactInfo = event => {
-      const property = event.target.name;
-      const value = event.target.value;
-
-      setContactInfo({
-          ...contactInfo,
-          [property]: value
-      })
-  }
-
-  const updateIsPasswordMatched = () => {
-    const isMatched = checkForPasswordMatch();
-
-    setIsPasswordMatched(isMatched);
-  }
-
-  const checkForCompleteForm = () => {
-    return !Object.values(contactInfo).some(value => value === "");
-  }
-
-  const checkForValidForm = () => {
-    let isValid = true;
-    if (!checkForCompleteForm() || !checkForPasswordMatch()) {
-      isValid = false;
-    }
-
-    return isValid;
-  }
-
-  const checkForPasswordMatch = () => {
-    let isMatched = false;
-    if (contactInfo.password === contactInfo.confirmPass) {
-      isMatched = true;
-    }
-
-    return isMatched;
   }
 
   const getCurrentStep = currentStep => setCurrentStep(currentStep);
---
+++ src/hooks/useContactInfo.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+const useContactInfo = () => {
+    const initialInfo = {
+        first: "",
+        last: "",
+        email: "",
+        password: "",
+        confirmPass: ""
+      }
+    
+    const [ contactInfo, setContactInfo ] = useState(initialInfo);
+    const [ isPasswordMatched, setIsPasswordMatched ] = useState(true);
+
+    const updateContactInfo = event => {
+        const property = event.target.name;
+        const value = event.target.value;
+  
+        setContactInfo({
+            ...contactInfo,
+            [property]: value
+        })
+    }
+  
+    const updateIsPasswordMatched = () => {
+      const isMatched = checkForPasswordMatch();
+  
+      setIsPasswordMatched(isMatched);
+    }
+  
+    const checkForCompleteForm = () => {
+      return !Object.values(contactInfo).some(value => value === "");
+    }
+  
+    const checkForValidForm = () => {
+      let isValid = true;
+      if (!checkForCompleteForm() || !checkForPasswordMatch()) {
+        isValid = false;
+      }
+  
+      return isValid;
+    }
+  
+    const checkForPasswordMatch = () => {
+      let isMatched = false;
+      if (contactInfo.password === contactInfo.confirmPass) {
+        isMatched = true;
+      }
+  
+      return isMatched;
+    }
+
+    return {
+        contactInfo,
+        updateContactInfo, 
+        isPasswordMatched,
+        updateIsPasswordMatched,
+        checkForValidForm
+    }
+}
+
+export default useContactInfo;
+
+
